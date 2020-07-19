ea3008bf60a17929bb13b9222201f69a28eddc82
Benjamin Jin <benjin@ethz.ch>
2020-07-19 23:38:24 +0200

    Clean up config file

--- calibration/utils/config.py
+++ calibration/utils/config.py
@@ -8,54 +8,10 @@ def command_line_parser():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    # For PcEdgeDetector
+    # General Arguments
     parser.add_argument(
-        '--pc_dir', type=str, default='',
-        help='Path to directory containing the point cloud files')
-
-    parser.add_argument(
-        '--pc_subsample', type=float, default=1.0,
-        help='Subsampling fraction for the pointcloud to reduce computation '
-             'while debugging')
-
-    parser.add_argument(
-        '--pc_ed_rad_nn', type=float, default=0.1,
-        help='Radius in which to include neighbors during point cloud edge '
-             'detection')
-
-    parser.add_argument(
-        '--pc_ed_num_nn', type=float, default=75,
-        help='Min number of nearest neighbors used')
-
-    parser.add_argument(
-        '--pc_ed_score_thr', type=float, default=60,
-        help='Percentile threshold above which points are considered edge '
-             'points')
-
-    parser.add_argument(
-        '--im_ed_method', type=str, default='sed',
-        help='Method used for image edge detection: sed or canny')
-
-    parser.add_argument(
-        '--im_ed_score_thr', type=float, default=0.25,
-        help='Threshold used for SED')
-
-    parser.add_argument(
-        '--im_ed_score_thr1', type=float, default=200,
-        help='Lower threshold for Canny')
-
-    parser.add_argument(
-        '--im_ed_score_thr2', type=float, default=300,
-        help='Upper threshold for Canny')
-
-    parser.add_argument(
-        '--img_dir', type=str, default='',
-        help='Path to directory containing the image files')
-
-    # For CameraLidarCalibrator
-    parser.add_argument(
-        '--calib_dir', type=str, default='',
-        help='Initial guess of the transformation')
+        '--dir', type=str, default='', required=True,
+        help='Path to directory containing point clouds, images and calibration')
 
     parser.add_argument(
         '--frames', type=json.loads, default='[1, 6, 19]',
@@ -76,6 +32,38 @@ def command_line_parser():
     parser.add_argument(
         '--sig_in', type=json.loads, default='[3.0 ,2.0 ,1.0]',
         help='Values for the refinement steps')
+
+    # Point Cloud Edge Detection Parameters
+    parser.add_argument(
+        '--pc_ed_rad_nn', type=float, default=0.1,
+        help='Radius in which to include neighbors during point cloud edge '
+             'detection')
+
+    parser.add_argument(
+        '--pc_ed_num_nn', type=float, default=75,
+        help='Min number of nearest neighbors used')
+
+    parser.add_argument(
+        '--pc_ed_score_thr', type=float, default=55,
+        help='Percentile threshold above which points are considered edge '
+             'points')
+
+    # Image Edge Detection Parameters
+    parser.add_argument(
+        '--im_ed_method', type=str, default='sed',
+        help='Method used for image edge detection: sed or canny')
+
+    parser.add_argument(
+        '--im_sed_score_thr', type=float, default=0.25,
+        help='Threshold used for SED')
+
+    parser.add_argument(
+        '--im_ced_score_lower_thr', type=float, default=100,
+        help='Lower threshold for Canny')
+
+    parser.add_argument(
+        '--im_ced_score_upper_thr', type=float, default=200,
+        help='Upper threshold for Canny')
 
     cfg = parser.parse_args()
 
